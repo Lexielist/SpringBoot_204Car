d17e57ed848a53923bc4e7ff7c2d1833fda95ad9
GBTC406010UR <GBTC406010UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-05-22 14:57:02 -0400

    innitial commit

---
+++ src/main/java/com/example/demo/SpringBoot204CarApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot204CarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot204CarApplication.class, args);
+	}
+
+}
